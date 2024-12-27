4789696b3ac31328540fe2eb0864788165b4a513
Xayrullo Ro'zimatov <xayrullorozimatov@Xayrullos-MacBook-Pro.local>
2024-12-27 21:31:47 +0500

    Added Testimonials Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 "use static"
 
 import AboutUs from "./AboutUsPart/AboutUs";
+import Testimonials from "./components/Testimonials/testimonials";
 import ImagesComp from "./ImagesComp";
 import Value_facts_part from "./value_facts_part/value_facts_part";
 import ValuePart from "./value_part/ValuePart";
+
 
 export default function Home() {
   return (
@@ -12,6 +14,7 @@ export default function Home() {
       <AboutUs/>
       <Value_facts_part/>
       <ImagesComp/>
+      <Testimonials/>
     </div>
   );
 }
