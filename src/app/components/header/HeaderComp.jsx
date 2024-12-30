b090f56b97900ce9674fa2f4e8ba5cf6614105fa
mrkadirov2005 <muzaffar571181@gmail.com>
2024-12-30 09:26:01 +0500

    translated header into uzbek

--- src/app/components/header/HeaderComp.jsx
+++ src/app/components/header/HeaderComp.jsx
@@ -19,10 +19,10 @@ export default function HeaderComp() {
     <header className={`flex  w-full items-center text-2xl justify-between rounded-md bg-gradient-to-tr from-fuchsia-800 to-blue-800  border-2 border-white p-4`}>
     
     <img src='/logo.jpg' className='w-10 h-10 rounded-md'></img>
-    <Link className='hover:scale-125' href={"/"}>Home</Link>
-    <Link className='hover:scale-125' href={"./../../courses"}  >Courses</Link>
-    <Link className='hover:scale-125' href={"../../events"}>Events</Link>
-    <Link className='hover:scale-125' href={"../../mocks"}>Mocks</Link>
+    <Link className='hover:scale-125' href={"/"}>Asosiy</Link>
+    <Link className='hover:scale-125' href={"./../../courses"}  >Kurslar</Link>
+    <Link className='hover:scale-125' href={"../../events"}>Tadbirlar</Link>
+    <Link className='hover:scale-125' href={"../../mocks"}>Mock imtihonlar</Link>
     <ContactButton/>
     </header>
   )
